53be683cdb7846db1282f96a3526667b44066a8f
Dean <dean.podgornik@gmail.com>
2015-01-20 23:02:17 +0100

    Added timestamp filename postfix

--- bck.sh
+++ bck.sh
@@ -11,13 +11,16 @@ HOST='localhost';
 #FILESYSTEM INFO
 #------------------
 EXPORT_FOLDER='./bck';
-EXPORT_PREFIX='test';
+EXPORT_PREFIX='test_';
 OLDER_THAN_DAYS=1;
 #------------------
 
+#RETRIEVE BACKUP FILE NAME
+DATE=`date +%Y%m%d%H%M%S`;
+EXPORT_FILENAME="$EXPORT_PREFIX$DATE.sql";
 
 #retrieve the backup
-mysqldump -u $USERNAME -p$PASSWORD $DATABASE -h $HOST > "$EXPORT_FOLDER/$EXPORT_PREFIX.sql"
+mysqldump -u $USERNAME -p$PASSWORD $DATABASE -h $HOST > "$EXPORT_FOLDER/$EXPORT_FILENAME"
 
 #delete the older backups
 find $EXPORT_FOLDER/*.sql -mtime +$OLDER_THAN_DAYS -exec rm {} \;
